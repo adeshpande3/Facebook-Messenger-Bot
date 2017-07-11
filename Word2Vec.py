1440754e44d2106884e870be06da0fb48f506e5f
Adit Deshpande <adeshpande3@ucla.edu>
2017-07-10 20:51:01 -0700

    Finished Word2Vec preprocessing code

--- Word2Vec.py
+++ Word2Vec.py
@@ -1,18 +1,44 @@
 import tensorflow as tf 
 import numpy as np 
+import re
+from collections import Counter
+import sys
+import pickle
+
+# This Word2Vec implementation is largely based on this paper
+# https://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality.pdf
+# It's a bit old, but Word2Vec is still SOTA and relatively simple, so I'm going with it
 
 wordVecDimensions = 100
 batchSize = 32
 numNegativeSample = 64
+windowSize = 5
 numIterations = 10000
 
 def cleanDataset(filename):
 	openedFile = open(filename, 'r')
 	allLines = openedFile.readlines()
+	myStr = ""
+	for line in allLines:
+	    tempStr = line.replace('\n',' ').lower()
+	    myStr += re.sub('[.!?]','', tempStr)
+	return myStr, Counter(myStr.split())
 
-def createTrainingMatrices(dataset):
-	allWords = set(dataset.split())
-	return allWords, xTrain, yTrain
+def createTrainingMatrices(dictionary, corpus):
+	allUniqueWords = dictionary.keys()	
+	allWords = corpus.split()
+	xTrain=[]
+	yTrain=[]
+	for i in range(len(allWords)):
+		wordsAfter = allWords[i+1:i+windowSize+1]
+		for word in wordsAfter:
+		    xTrain.append(allUniqueWords.index(allWords[i]))
+		    yTrain.append(allUniqueWords.index(word))
+		wordsBefore = allWords[max(0, i-windowSize):i]
+		for word in wordsBefore:
+		    xTrain.append(allUniqueWords.index(allWords[i]))
+		    yTrain.append(allUniqueWords.index(word))
+	return allUniqueWords, xTrain, yTrain
 
 def getTrainingBatch():
 	num = randint(0,numTrainingExamples - batchSize)
@@ -20,10 +46,23 @@ def getTrainingBatch():
 	labels = yTrain[num:num + batchSize]
 	return arr, labels
 
-cleanedDataset = cleanDataset('ConversationData.txt')
-wordList, xTrain, yTrain  = createTrainingMatrices(cleanedDataset)
+if (os.path.isfile('xTrain.npy') and os.path.isfile('yTrain.npy') and os.path.isfile('wordList.txt'):
+	xTrain = np.load('xTrain.npy')
+	yTrain = np.load('yTrain.npy')
+	with open("wordList.txt", "rb") as fp: 
+		allUniqueWords = pickle.load(fp)
+
+fullCorpus, datasetDictionary = cleanDataset('ConversationData.txt')
+wordList, xTrain, yTrain  = createTrainingMatrices(datasetDictionary, fullCorpus)
 numTrainingExamples = len(xTrain)
 vocabSize = len(wordList)
+
+np.save('xTrain.npy', xTrain)
+np.save('yTrain.npy', yTrain)
+with open("wordList.txt", "wb") as fp: 
+	pickle.dump(allUniqueWords, fp)
+
+sys.exit()
 
 sess = tf.Session()
 embeddingMatrix = tf.Variable(tf.random_uniform([vocabSize, wordVecDimensions], -1.0, 1.0))
@@ -47,6 +86,6 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=1.0).minimize(loss)
 
 for i in range(numIterations):
 	trainInputs, trainLabels = getTrainingBatch()
-	_, curLoss = session.run([optimizer, loss], feed_dict={inputs = trainInputs, outputs = trainLabels})
+	_, curLoss = sess.run([optimizer, loss], feed_dict={inputs: trainInputs, outputs: trainLabels})
 	if (i % 100 == 0):
 		print ('Current loss is:', curLoss)
