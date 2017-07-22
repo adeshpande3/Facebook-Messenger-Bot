354f7318d4ec31c0b66034cf590dbbb368eb0c79
Adit Deshpande <adeshpande3@ucla.edu>
2017-07-22 16:06:20 -0700

    Forgot to add myID

--- index.js
+++ index.js
@@ -26,11 +26,14 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 })
 
+const myID = 1960455644201170;
+
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
 	    let sender = event.sender.id
+
 	    if (event.message && event.message.text && sender != myID) {
 	    	let text = event.message.text
 			request({
